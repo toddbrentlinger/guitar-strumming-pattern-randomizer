cd1eaa1ce198428a129b77e06f24c67c8c66e14d
Todd Brentlinger <toddbrentlinger@gmail.com>
2020-11-14 16:17:02 -0800

    Updates

---
+++ precache-manifest.bc565cf08298e46012916c99b367ff6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48ec39b5fcd92c8a3aa62972147b39d",
+    "url": "/guitar-strumming-pattern-randomizer/index.html"
+  },
+  {
+    "revision": "1743f41f7f9d75a62d48",
+    "url": "/guitar-strumming-pattern-randomizer/static/css/main.394c2136.chunk.css"
+  },
+  {
+    "revision": "8d80123feb815299bc82",
+    "url": "/guitar-strumming-pattern-randomizer/static/js/2.f9ee378e.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/guitar-strumming-pattern-randomizer/static/js/2.f9ee378e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1743f41f7f9d75a62d48",
+    "url": "/guitar-strumming-pattern-randomizer/static/js/main.a246193d.chunk.js"
+  },
+  {
+    "revision": "a70f3a44017a2a479f98",
+    "url": "/guitar-strumming-pattern-randomizer/static/js/runtime-main.6039dbcc.js"
+  },
+  {
+    "revision": "df6a356d5dff12b01149454cd7fbbcfc",
+    "url": "/guitar-strumming-pattern-randomizer/static/media/justin-guitar-header-logo.df6a356d.png"
+  }
+]);
